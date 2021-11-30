98dd4791108b774d31aeac5ecfa5d85ffb7c15d1
Jialiang Han <jialianghan@bitorobotics.ltd>
2021-11-30 13:51:16 +0800

    ✨ feat(ch3.h, ch3.cpp): ch3 finished

--- include/ch3.h
+++ include/ch3.h
@@ -1,12 +1,114 @@
-void ConvertInchesToFeet();
+#include <iostream>
+namespace ch3
+{
 
-float ConvertHeightToInches(const int &feet, const int &inches);
+    const int NumInchesInFeet = 12;
+    const float NumMetersInInches = 0.0254;
+    const float NumPoundsInKg = 2.2;
+    const int NumSecondsInArc = 60;
+    const int NumArcInDegree = 60;
 
-float ConvertInchesToMeters(const int &inches);
+    void ConvertInchesToFeet();
 
-float ConvertPoundsToKg(const float &pounds);
+    float ConvertHeightToInches(const int &feet, const int &inches);
 
-float CalculateBMI(const float &weight_in_kg, const float &height_in_meters);
+    float ConvertInchesToMeters(const int &inches);
 
-float ConvertMinutesToDegree(const float &arc);
-float ConvertSecondsToMinutes(const float &seconds);
+    float ConvertPoundsToKg(const float &pounds);
+
+    float CalculateBMI(const float &weight_in_kg, const float &height_in_meters);
+
+    float ConvertMinutesToDegree(const float &arc);
+    float ConvertSecondsToMinutes(const float &seconds);
+
+    class exercise
+    {
+    public:
+        virtual void Initial() = 0;
+        virtual void DoExercise() = 0;
+        virtual void Output() = 0;
+    };
+
+    class exercise_4 : public exercise
+    {
+    public:
+        exercise_4()
+        {
+            Initial();
+            DoExercise();
+            Output();
+            std::cout << "Exercise 4 is done." << std::endl;
+        };
+        void Initial();
+        void DoExercise();
+        void Output();
+
+    private:
+        long total_;
+        int seconds_;
+        int minutes_;
+        int hours_;
+        int days_;
+        const int NumHoursInDay = 24;
+        const int NumMinutesInHour = 60;
+        const int NumSecondsInMinute = 60;
+    };
+    class exercise_5 : public exercise
+    {
+    public:
+        exercise_5()
+        {
+            Initial();
+            DoExercise();
+            Output();
+            std::cout << "Exercise 5 is done." << std::endl;
+        };
+        void Initial();
+        void DoExercise();
+        void Output();
+
+    private:
+        long long world_population_;
+        long long us_population_;
+        float percentage_;
+    };
+    class exercise_6 : public exercise
+    {
+    public:
+        exercise_6()
+        {
+            Initial();
+            DoExercise();
+            Output();
+            std::cout << "Exercise 6 is done." << std::endl;
+        };
+        void Initial();
+        void DoExercise();
+        void Output();
+
+    private:
+        long long miles_driven_;
+        long long gasline_used_;
+        float mile_per_gallon_;
+    };
+    class exercise_7 : public exercise
+    {
+    public:
+        exercise_7()
+        {
+            Initial();
+            DoExercise();
+            Output();
+            std::cout << "Exercise 7 is done." << std::endl;
+        };
+        void Initial();
+        void DoExercise();
+        void Output();
+
+    private:
+        float liters_per_100_km_;
+        float mile_per_gallon_;
+        const float NumMilesIn100Km_ = 62.14;
+        const float NumLitersInGallon = 3.875;
+    };
+}
--- src/ch3.cpp
+++ src/ch3.cpp
@@ -1,14 +1,8 @@
 #include "ch3.h"
-#include <iostream>
-const int NumInchesInFeet = 12;
-const float NumMetersInInches = 0.0254;
-const float NumPoundsInKg = 2.2;
-const int NumSecondsInArc = 60;
-const int NumArcInDegree = 60;
 
-void ConvertInchesToFeet()
+void ch3::ConvertInchesToFeet()
 {
-    std::cout << "Please enter your height in inches:";
+    std::cout << "Enter your height in integer inches: ___\b\b\b";
     int inches;
     std::cin >> inches;
     int feet = inches / NumInchesInFeet;
@@ -16,34 +10,108 @@ void ConvertInchesToFeet()
     std::cout << "your height is :" << feet << " feet and " << inches << " inches" << std::endl;
 }
 
-float ConvertHeightToInches(const int &feet, const int &inches)
+float ch3::ConvertHeightToInches(const int &feet, const int &inches)
 {
     return feet * NumInchesInFeet + inches;
 }
 
-float ConvertInchesToMeters(const int &inches)
+float ch3::ConvertInchesToMeters(const int &inches)
 {
     return inches * NumMetersInInches;
 }
 
-float ConvertPoundsToKg(const float &pounds)
+float ch3::ConvertPoundsToKg(const float &pounds)
 {
     return pounds / NumPoundsInKg;
 }
 
-float CalculateBMI(const float &weight_in_kg, const float &height_in_meters)
+float ch3::CalculateBMI(const float &weight_in_kg, const float &height_in_meters)
 {
     return weight_in_kg / height_in_meters / height_in_meters;
 }
 
-float ConvertMinutesToDegree(const float &arc)
+float ch3::ConvertMinutesToDegree(const float &arc)
 {
     return arc / NumArcInDegree;
 }
-float ConvertSecondsToMinutes(const float &seconds)
+float ch3::ConvertSecondsToMinutes(const float &seconds)
 {
     return seconds / NumSecondsInArc;
 }
+
+void ch3::exercise_4::Initial()
+{
+    std::cout << "Enter the number of secondes: ___\b\b\b";
+    std::cin >> total_;
+}
+
+void ch3::exercise_4::DoExercise()
+{
+    days_ = total_ / NumSecondsInMinute / NumMinutesInHour / NumHoursInDay;
+    hours_ = total_ / NumSecondsInMinute / NumMinutesInHour % NumHoursInDay;
+    minutes_ = total_ / NumSecondsInMinute % NumMinutesInHour;
+    seconds_ = total_ % NumSecondsInMinute;
+}
+
+void ch3::exercise_4::Output()
+{
+    std::cout << total_ << " seconds = " << days_ << " days, " << hours_ << " hours, " << minutes_ << " minutes, " << seconds_ << " seconds." << std::endl;
+}
+
+void ch3::exercise_5::Initial()
+{
+    std::cout << "Enter world population: ___\b\b\b";
+    std::cin >> world_population_;
+    std::cout << "Enter us population: ___\b\b\b";
+    std::cin >> us_population_;
+}
+
+void ch3::exercise_5::DoExercise()
+{
+
+    percentage_ = (float)us_population_ / world_population_ * 100;
+}
+
+void ch3::exercise_5::Output()
+{
+    std::cout << "teh population of the US is " << percentage_ << "% of the world population." << std::endl;
+}
+void ch3::exercise_6::Initial()
+{
+    std::cout << "Enter the number of mils you have driven: ___\b\b\b";
+    std::cin >> miles_driven_;
+    std::cout << "Enter the number of gasline you have used: ___\b\b\b";
+    std::cin >> gasline_used_;
+}
+
+void ch3::exercise_6::DoExercise()
+{
+
+    mile_per_gallon_ = (float)miles_driven_ / gasline_used_;
+}
+
+void ch3::exercise_6::Output()
+{
+    std::cout << "mile per gallon is " << mile_per_gallon_ << std::endl;
+}
+
+void ch3::exercise_7::Initial()
+{
+    std::cout << "Enter the number of liters per 100 kilometers: ___\b\b\b";
+    std::cin >> liters_per_100_km_;
+}
+
+void ch3::exercise_7::DoExercise()
+{
+
+    mile_per_gallon_ = 1 * NumMilesIn100Km_ / (liters_per_100_km_ / NumLitersInGallon);
+}
+
+void ch3::exercise_7::Output()
+{
+    std::cout << "mile per gallon is " << mile_per_gallon_ << std::endl;
+}
+
 int main()
 {
     // ConvertInchesToFeet();
@@ -55,13 +123,14 @@ int main()
     // std::cout << "Please enter your weight in pounds";
     // std::cin >> weight_in_pounds;
     // std::cout << "your BMI is: " << CalculateBMI(ConvertPoundsToKg(weight_in_pounds), ConvertInchesToMeters(ConvertHeightToInches(feet, inches))) << std::endl;
-    float degrees, minutes, seconds;
-    std::cout << "Enter a latitude in degrees,minutes, and seconds:" << std::endl;
-    std::cout << "First, enter the degrees:";
-    std::cin >> degrees;
-    std::cout << "Next, enter the minutes of arc:";
-    std::cin >> minutes;
-    std::cout << "Finally, enter the seconds of arc:";
-    std::cin >> seconds;
-    std::cout << degrees << " degrees, " << minutes << " minutes, " << seconds << " seconds = " << degrees + ConvertMinutesToDegree(minutes + ConvertSecondsToMinutes(seconds)) << " degrees" << std::endl;
+    // float degrees, minutes, seconds;
+    // std::cout << "Enter a latitude in degrees,minutes, and seconds:" << std::endl;
+    // std::cout << "First, enter the degrees:";
+    // std::cin >> degrees;
+    // std::cout << "Next, enter the minutes of arc:";
+    // std::cin >> minutes;
+    // std::cout << "Finally, enter the seconds of arc:";
+    // std::cin >> seconds;
+    // std::cout << degrees << " degrees, " << minutes << " minutes, " << seconds << " seconds = " << degrees + ConvertMinutesToDegree(minutes + ConvertSecondsToMinutes(seconds)) << " degrees" << std::endl;
+    ch3::exercise_7 ex7;
 }
